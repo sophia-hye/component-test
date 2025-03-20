056dc36be16f3b900b8ff8ee176f49b20eaacad7
sophia <kimjihae28@gmail.com>
2025-03-20 17:15:35 +0900

    feat: test scrollable container

--- src/components/templates/test/TestCommonUI.tsx
+++ src/components/templates/test/TestCommonUI.tsx
@@ -2,25 +2,33 @@ import React from 'react';
 import Input from '@/commonui/Input';
 import Divider from '@/commonui/Divider';
 import Selectbox from '@/commonui/Selectbox';
+import ScrollableContainer from '@/commonui/ScrollableContainer';
 
 export default function TestCommonUI() {
   return (
     <div>
       <h3>CommonUI Test</h3>
-      <div style={{ display: 'flex', gap: 10 }}>
-        <Input placeholder="placeholder" />
-        <Divider direction="vertical" />
-        <Divider direction="vertical" length={20} />
-        <Input disabled placeholder="disabled" />
-      </div>
-      <br />
-      <Divider direction="horizontal" /> <br />
-      <Divider direction="horizontal" length={20} />
-      <br />
-      <div style={{ display: 'flex', gap: 10, width: '200px' }}>
-        <Selectbox />
-      </div>
-      <br />
+      <ScrollableContainer>
+        <div style={{ display: 'flex', gap: 10 }}>
+          <Input placeholder="placeholder" />
+          <Divider direction="vertical" length={20} />
+          <Input placeholder="disabled" disabled />
+        </div>
+        <br />
+        <Divider direction="horizontal" />
+        <br />
+        <div style={{ display: 'flex', gap: 10 }}>
+          <Input placeholder="test" />
+          <Divider direction="vertical" />
+          <Input placeholder="test2" />
+        </div>
+        <br />
+        <Divider direction="horizontal" length={20} />
+        <br />
+        <div style={{ display: 'flex', gap: 10, width: '200px' }}>
+          <Selectbox />
+        </div>
+      </ScrollableContainer>
       <br />
       <br />
       <br />
